a138ca4b60003f0386ed491b02500d1ef831a3e9
Justina Stankiene <jstanki@corp.transunion.com>
2022-04-21 21:08:15 +0300

    Add project files.

---
+++ ClassLibrary1/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Calculator 
+    {
+        public double Sum(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
