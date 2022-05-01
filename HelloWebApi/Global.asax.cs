684c5de9360369dff778b0294e2bbad2c37182ef
Justina Stankiene <jstanki@corp.transunion.com>
2022-05-01 20:00:26 +0300

    Hello API

---
+++ HelloWebApi/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace HelloWebApi
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configuration.Routes.Add("default", new HttpRoute("{controller}"));
+        }
+
+    }
+}
---
+++ HelloWebApi/HelloApiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace HelloWebApi
+{
+    public class HelloApiController : ApiController
+    {
+        public HttpResponseMessage Get(string name)
+        {
+            string result = $"Hello, {name}";
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            resp.Content = new StringContent(result, System.Text.Encoding.UTF8, "text/plain");
+            return resp;
+        }
+    }
+}
